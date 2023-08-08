a7b43cfef2af48b3e71b5af7250b4020ed55302e
ksohui13 <sohui0103@naver.com>
2023-08-08 14:42:10 +0900

    admin.py 카테고리 추가

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product, ProductQuest, QuestComment
+from .models import Product, ProductQuest, QuestComment, Category
 
 # Register your models here.
+admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(ProductQuest)
 admin.site.register(QuestComment)
