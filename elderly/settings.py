0fb4f653dfcd158dc6757369eb1b9e6775bb347e
ksohui13 <sohui0103@naver.com>
2023-08-08 14:10:46 +0900

    배포용settings.py설정

--- elderly/settings.py
+++ elderly/settings.py
@@ -11,13 +11,13 @@ pymysql.install_as_MySQLdb()
 BASE_DIR = Path(__file__).resolve().parent.parent
 ROOT_DIR = os.path.dirname(BASE_DIR)
 
-SECRET_BASE_FILE = os.path.join(BASE_DIR, 'secrets.json')
+# SECRET_BASE_FILE = os.path.join(BASE_DIR, 'secrets.json')
 
-secrets = json.loads(open(SECRET_BASE_FILE).read())
-for key, value in secrets.items():
-    setattr(sys.modules[__name__], key, value)
+# secrets = json.loads(open(SECRET_BASE_FILE).read())
+# for key, value in secrets.items():
+#     setattr(sys.modules[__name__], key, value)
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -95,26 +95,26 @@ TEMPLATES = [
 WSGI_APPLICATION = 'elderly.wsgi.application'
 
 
-secret_file = os.path.join(BASE_DIR, 'secrets.json')
+# secret_file = os.path.join(BASE_DIR, 'secrets.json')
 
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
+# with open(secret_file) as f:
+#     secrets = json.loads(f.read())
 
 
 # 환경변수 분리
-#def get_env_variable(var_name):
-#    try:
-#      return os.environ[var_name]
-#    except KeyError:
-#      error_msg = 'Set the {} environment variable'.format(var_name)
-#      raise ImproperlyConfigured(error_msg)
-
-def get_env_variable(key):
+def get_env_variable(var_name):
    try:
-       return secrets[key]
+     return os.environ[var_name]
    except KeyError:
-       error_msg = f"Set the {key} environment variable"
-       raise ImproperlyConfigured(error_msg)
+     error_msg = 'Set the {} environment variable'.format(var_name)
+     raise ImproperlyConfigured(error_msg)
+
+# def get_env_variable(key):
+#    try:
+#        return secrets[key]
+#    except KeyError:
+#        error_msg = f"Set the {key} environment variable"
+#        raise ImproperlyConfigured(error_msg)
 
 
 SECRET_KEY = get_env_variable('SECRET_KEY')
